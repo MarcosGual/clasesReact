a5b3683f49d9f5904b0d885010f04929982c57e6
Marcos Gual <111923@tecnicatura.frc.utn.edu.ar>
2022-07-13 13:15:24 -0300

    agregando vainilla js

--- primerReact/index.js
+++ primerReact/index.js
@@ -6,38 +6,55 @@
 // 2- el elemento raíz o contenedor donde quiero renderizarlo
 // parecido al método appendChild()
 
-function Bienvenida() {
-  return <h1>Bienvenidos a React!</h1>;
-}
+// function Bienvenida() {
+//   return <h1>Bienvenidos a React!</h1>;
+// }
 
-function BarraNav() {
-  return (
-    <nav>
-      <ul class="nav">
-        <li>Inicio</li>
-        <li>Recetas</li>
-        <li>Sobre Nosotros</li>
-      </ul>
-    </nav>
-  );
-}
+// function BarraNav() {
+//   return (
+//     <nav>
+//       <ul className="nav">
+//         <li>Inicio</li>
+//         <li>Recetas</li>
+//         <li>Sobre Nosotros</li>
+//       </ul>
+//     </nav>
+//   );
+// }
 
-function Lista() {
-  return (
-    <ul>
-      <li>Pan</li>
-      <li>Salchichas</li>
-      <li>Mayonesa</li>
-      <li>Mostaza</li>
-    </ul>
-  );
-}
+// function Titulo(){
+//   return (
+//     <h2 className="subtitulo">Lista de Compras</h2>
+//   );
+// }
 
-ReactDOM.render(
-  <div>
-    <Bienvenida />
-    <BarraNav />
-    <Lista />
-  </div>,
-  document.querySelector("#raiz")
-);
+// function Lista() {
+//   return (
+//     <ul>
+//       <li>Pan</li>
+//       <li>Salchichas</li>
+//       <li>Mayonesa</li>
+//       <li>Mostaza</li>
+//     </ul>
+//   );
+// }
+
+// ReactDOM.render(
+//   <div>
+//     <Bienvenida />
+//     <BarraNav />
+//     <Titulo />
+//     <Lista />
+//   </div>,
+//   document.querySelector("#raiz")
+// );
+
+//Con Javascript
+
+const $contenedor = document.querySelector("#raiz");
+const $h1 = document.createElement("h1");
+$h1.textContent = "Forma imperativa de programar";
+$h1.classList.add("header");
+$contenedor.appendChild($h1);
+
+// document.getElementById('raiz').append($h1);
