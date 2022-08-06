261925ea7e632669d051680c0da5a7b23bb3c998
MarcosGual <marcos.gual@outlook.com>
2022-08-06 14:05:45 -0300

    componente itemlista agregado

--- react-app/mi-app/src/App.js
+++ react-app/mi-app/src/App.js
@@ -1,21 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import ListaUsuarios from './components/ListaUsuarios';
-
-function ItemLista({ id, contenido, deleteItem }) {
-
-  function eventHandler() {
-    // console.log(contenido);
-    // console.log('Estoy en el manejador de eventos y este es el id: ', id);
-    deleteItem(id);
-  }
-
-  return (
-    <li key={id} onClick={eventHandler}>
-      {contenido}
-    </li>
-  )
-}
+import ItemLista from './components/ItemLista';
 
 function App() {
 
