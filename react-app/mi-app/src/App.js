c93175b962573d6c4534faaad162b99de24dafcb
MarcosGual <marcos.gual@outlook.com>
2023-03-18 01:47:10 -0300

    delete: components

--- react-app/mi-app/src/App.js
+++ react-app/mi-app/src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
-import NavBar from "./components/NavBar";
-import Presentacion from "./components/Presentacion";
-import Footer from "./components/Footer";
-import ContextoTemaProvider from "./providers/ContextoTemaProvider";
 
 function App() {
   return (
     <>
-      <ContextoTemaProvider>
-        <NavBar />
-        <Header />
-        <Presentacion />
-        <Footer />
-      </ContextoTemaProvider>
+      <h1>Clase 3 React: Estado</h1>
     </>
   );
 }
