fe433a1afdd6f5a12799b7c047a083cdd2189da9
MarcosGual <marcos.gual@outlook.com>
2023-12-21 21:40:45 -0300

    add: login

--- pruebas-react/src/App.js
+++ pruebas-react/src/App.js
@@ -1,14 +1,28 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Carrito from "./components/Carrito";
-import ContextoTemaProvider from "./providers/contextoTemaProvider";
-
+import Main from "./components/Main";
+import Preferencias from "./components/Preferencias";
+import Login from "./components/Login";
+import useToken from "./hooks/useToken";
 
 function App() {
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
 
   return (
-    <>
-
-    </>
+    <div className="wrapper">
+      <h1>Aplicación</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/main" />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/preferencias" element={<Preferencias />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ pruebas-react/src/components/Preferencias.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Preferencias() {
+  return(
+    <h2>Preferencias</h2>
+  );
+}
